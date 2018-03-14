38135ee710e1faf22768016c641c71dd1b37d2ab
Ainuddin Faizan <andyfaizan@gmail.com>
2018-03-14 16:49:19 +0100

    Removed counter

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
-import counter from './counter';
 import data from './data';
 
 const rootReducer = combineReducers({
-  counter,
   data,
   routing
 });
